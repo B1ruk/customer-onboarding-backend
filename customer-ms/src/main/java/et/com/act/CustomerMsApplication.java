27d43c4b18c31fe8347464d6cb7c3bb46a910802
b1ruk <biruk3870@gmail.com>
2024-07-27 09:40:28 -0300

    add sidecar for the customer node microservice app

---
+++ customer-ms/src/main/java/et/com/act/CustomerMsApplication.java
@@ -0,0 +1,14 @@
+package et.com.act;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+
+@EnableSidecar
+@SpringBootApplication
+public class CustomerMsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerMsApplication.class, args);
+    }
+}
