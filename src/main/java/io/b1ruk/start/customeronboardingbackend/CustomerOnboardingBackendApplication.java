bb1cac4b600af144673243049966aeffc1532e77
b1ruk <biruk3870@gmail.com>
2024-07-27 08:30:14 -0300

    Multi-Module Project for backend microservice
    - spring cloud dependencies
    - initial project structure
    - add git url for the config server
    - add s3 & sqs dependencies

---
+++ src/main/java/io/b1ruk/start/customeronboardingbackend/CustomerOnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package io.b1ruk.start.customeronboardingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOnboardingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerOnboardingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/b1ruk/start/customeronboardingbackend/CustomerOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.b1ruk.start.customeronboardingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerOnboardingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
